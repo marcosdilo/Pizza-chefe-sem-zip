9bb64a225bd96a877f2af5bd70b914e356d81d72
marcosdilo <dilooqa.f@gmail.com>
2026-04-06 09:58:23 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,26 @@
+<?php
+// CONECTA A MERDA DO XAMPP AQUI
+$conn = new mysqli_connect()
+if($conn->connect_error) {
+    die("Erro na conexão: " . $conn->connect_error)
+}
+
+// regisrar a bomba
+
+$nome = $_POST['nome'];
+$email = $_POST['senha'];
+$senha = $_POST['senha'];
+
+//sql
+$sql = "INSERT INTO usuarios (nome, email, senha) VALUES ( '$nome', '$email', '$senha')";
+
+//exec
+if ($conn->query($sql) === TRUE) {
+        echo "Usuário registrado com sucesso!";
+} else {
+        echo "Erro ao registrar: " . $conn->error;
+}
+
+// fechar a conexão
+$conn->close();
+?>
