14fabfee76b8376a8bb8454107402c92a9bc2c33
marcosdilo <dilooqa.f@gmail.com>
2026-04-06 10:36:40 -0300

    Atualizar o validacao.js

--- validacao.js
+++ validacao.js
@@ -1,3 +1,27 @@
 const formulario = document.getElementById("meuForm");
 formulario.addEventListener("submit", function(evento) {
+    
+evento.preventDefault();
+
+const nomeUsuario = document.getElementById("nome").value.trim();
+const emailUsuario = document.getElementById("email").value;
+const senhaUsuario = document.getElementById("senha").value;
+    
+if (nomeUsuario === "") {
+    alert("Por favor, preencha o seu nome.");
+    return;
+}
+
+if (!emailUsuario.includes("@")) {
+        alert("Por favor, coloque um e-mail válido!");
+        return;
+}
+
+if (senhaUsuario.length < 6) {
+    alert("A senha deve conter pelo menos 6 caracteres!");
+    return;
+}
+
+alert("Tudo certo! Enviando seus dados...");
+formulario.submit(); 
 });
